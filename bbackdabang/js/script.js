5e2d7b58d731b974f292c6df6ce7d3a7baf49761
Jeongseo <ohjs652@gmail.com>
2025-06-30 17:40:54 +0900

    0630academy

--- bbackdabang/js/script.js
+++ bbackdabang/js/script.js
@@ -1,26 +1,24 @@
 $(function(){
+
     //header navigation
     let one_li = $('.nav>ul>li')
-
     one_li.hover(
         function () {
         // VoiceofCustomer 클래스는 제외
             if (!$(this).hasClass('VoiceofCustomer') && $(this).find('ul').length) {
                 // 현재 li의 ul만 보여줌
-                $(this).find('ul').stop(true, true).slideDown(400);
-        
                 // 배경 .sub-bg에 .on 클래스 추가
                 $('.sub-bg').addClass('on');
+                $(this).find('ul').stop(true, true).slideDown(400);
             }
         },
         function () {
             // VoiceofCustomer 클래스는 제외
             if (!$(this).hasClass('VoiceofCustomer') && $(this).find('ul').length) {
                 // 현재 li의 ul만 숨김
-                $(this).find('ul').stop(true, true).slideUp(400);
-
                 // 배경 .sub-bg에 .on 클래스 제거
                 $('.sub-bg').removeClass('on');
+                $(this).find('ul').stop(true, true).slideUp(400);
             }
         }
     );
@@ -31,4 +29,18 @@ $(function(){
         el: ".swiper-pagination",
       },
     });
+
+    //footer familysite
+    var familysite_Button = $('.familyBtn');
+    var familysite_UL = $('.familysiteUL')
+    var family_slide=0;
+    familysite_Button.click(function(){
+        if(family_slide == 0){
+            familysite_UL.slideUp(200);
+            family_slide=1;
+        }else if(family_slide == 1){
+            familysite_UL.slideDown(200);
+            family_slide=0;
+        }
+    })
 })
